c103be8e3e5d28e972a9fc7db8041efc0606e365
Aleksandr <exte92@gmail.com>
2023-06-27 15:37:46 +0300

    add logout

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -11,7 +11,19 @@ const Main = () => {
         if (!token) nav('/auth');
     }, []);
 
-    return <main className='main'>Main</main>
+    return <main className='main'>
+        <div className='main__header'>
+            <span className='main__header_logo'>Logo</span>
+            <button
+                onClick={() => {
+                    localStorage.removeItem('testAuthorization');
+                    nav('/auth');
+                }}
+                className='main__header_button'>
+                Logout
+            </button>
+        </div>
+    </main>
 };
 
 export default Main;
